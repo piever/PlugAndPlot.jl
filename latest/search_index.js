8d3a010194ff1b3ef1e729abf92f9cc0784a7f47
autodocs <autodocs>
2017-08-09 20:16:07 +0000

    build based on efe20bf

---
+++ latest/search_index.js
@@ -0,0 +1,43 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#PlugAndPlot.jl-1",
+    "page": "Home",
+    "title": "PlugAndPlot.jl",
+    "category": "section",
+    "text": "GUI to create statistical plots from a DataFrame, using DataFrames, QML and StatPlots"
+},
+
+{
+    "location": "index.html#Installation-1",
+    "page": "Home",
+    "title": "Installation",
+    "category": "section",
+    "text": "The package is not yet registered in Julia's package registry, and so it must be installed usingPkg.clone(\"https://github.com/piever/PlugAndPlot.jl.git\")"
+},
+
+{
+    "location": "index.html#PlugAndPlot.build_window",
+    "page": "Home",
+    "title": "PlugAndPlot.build_window",
+    "category": "Function",
+    "text": "build_window(; kwargs...)\n\nStarts the GUI asking for a suitable csv file.\n\n\n\nbuild_window(datafile::AbstractString; kwargs...)\n\nReads a csv file and starts build_window on the corresponding DataFrame\n\n\n\nbuild_window(dataset::AbstractDataFrame; nbox = 5)\n\nCreates a GUI to analyze a DataFrame interactively. Data can be selected either on continuous columns, with SpinBoxes or on discrete columns with checkboxes, provided there are less than nbox entries.\n\n\n\n"
+},
+
+{
+    "location": "index.html#Documentation-1",
+    "page": "Home",
+    "title": "Documentation",
+    "category": "section",
+    "text": "build_window"
+},
+
+]}
