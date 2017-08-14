99e28ed7eba55c8a241ae14ab2e7a3dd118be664
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2017-08-15 00:12:00 +0100

    move docs to linux

--- docs/make.jl
+++ docs/make.jl
@@ -12,7 +12,7 @@ deploydocs(
     repo = "github.com/piever/PlugAndPlot.jl.git",
     target = "build",
     julia  = "0.6",
-    osname = "osx",
+    osname = "linux",
     deps   = nothing,
     make   = nothing
 )
