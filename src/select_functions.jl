b958e026fc5633c3006b2e9fbe8d2d4e99f8767f
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2017-08-14 23:52:45 +0100

    back to previous QML

--- src/select_functions.jl
+++ src/select_functions.jl
@@ -12,21 +12,21 @@ end
 function choose_data(a,d)
     index = broadcast(t -> true, 1:(size(a,1)))
     for key in keys(d)
-        index .= combine(index, d[key],a[key])
+        combine!(index, d[key],a[key])
     end
     return a[index,:]
 end
 
-function combine(index, func::Function, values)
-    return index .& broadcast(func,values)
+function combine!(index, func::Function, values)
+    @. index = index & func(values)
 end
 
-function combine{T}(index, els::AbstractArray{T,1}, values)
+function combine!{T}(index, els::AbstractArray{T,1}, values)
     func = t -> (string(t) in els)
-    return combine(index, func, values)
+    combine!(index, func, values)
 end
 
-function combine(index, el, values)
+function combine!(index, el, values)
     func = t -> (t == el)
-    return combine(index, func, values)
+    return combine!(index, func, values)
 end
