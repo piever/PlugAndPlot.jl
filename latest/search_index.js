b9e5db7ab5a037d925c504671f904aeec376b86c
autodocs <autodocs>
2017-10-02 21:18:46 +0000

    build based on 504f233

--- latest/search_index.js
+++ latest/search_index.js
@@ -21,7 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Introduction",
     "title": "Overview",
     "category": "section",
-    "text": "PlugAndPlot.jl allows to easily create and deploy a Graphical User Interface to draw statistical plots from a DataFrame interactively. Once a DataFrame is loaded, PlugAndPlot.jl will create a GUI with widgets corresponding to the different functionalities of StatPlots.jl. A second layer of widget simplifies data selection and splitting. Once the plot is created, it can be modified using the usual Plots.jl keywords.Here is a screenshot from an example dataset:(Image: )"
+    "text": "PlugAndPlot.jl allows to easily create and deploy a Graphical User Interface to draw statistical plots from a DataFrame interactively. Once a DataFrame is loaded, PlugAndPlot.jl will create a GUI with widgets corresponding to the different functionalities of StatPlots.jl and GroupedErrors.jl. A second layer of widget simplifies data selection and splitting. Once the plot is created, it can be modified using the usual Plots.jl keywords.Here is a screenshot from an example dataset:(Image: gui)"
 },
 
 {
@@ -37,7 +37,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Introduction",
     "title": "Known issues",
     "category": "section",
-    "text": "Pressing Tab can cause a segmentation fault. The issue is being investigate, but for the time being it is recommended to not press Tab while using PlugAndPlot.jl.\nPlotting on top of a previous plot (the button Plot!) is experimental and can behave strangely in combination with adding keywords or using sliders."
+    "text": "Plotting on top of a previous plot (the button Plot!) is experimental and can behave strangely in combination with adding keywords or using sliders."
 },
 
 {
@@ -73,6 +73,62 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "getting_started.html#Choosing-the-analysis-1",
+    "page": "Getting Started",
+    "title": "Choosing the analysis",
+    "category": "section",
+    "text": "The choice of a statistical analysis is just the same as in GroupedErrors. Below it is explained step by step."
+},
+
+{
+    "location": "getting_started.html#Selecting-the-x-variable-1",
+    "page": "Getting Started",
+    "title": "Selecting the x variable",
+    "category": "section",
+    "text": "It must be a column of your dataset."
+},
+
+{
+    "location": "getting_started.html#Selecting-the-y-variable-1",
+    "page": "Getting Started",
+    "title": "Selecting the y variable",
+    "category": "section",
+    "text": "It can be either a column of your dataset or an analysis to effectuate on the x variable (for example, computing its densiy or hazard)."
+},
+
+{
+    "location": "getting_started.html#Deciding-the-axis-type-1",
+    "page": "Getting Started",
+    "title": "Deciding the axis type",
+    "category": "section",
+    "text": "Axis type pointbypoint is needed when you want to plot x against y, and it only makes sense when y is also a column of your data. The widget dataperpoint asks whether you want to split-apply-combine your data before plotting. The pointbypoint option means: \"plot as is\". Otherwise, it is possible to group by one of your data columns and only plot some summary variable (e.g. the mean). The summary function can be defined below the x axis and y axis widgets. mean is the default. To also get an estimate of the variability, you can put a tuple of two functions, the first being the average and the second the variability, i.e. (mean, sem).Axis type continuous is recommended when dealing with a continuous x variable. The built-in analysis functions will then use their continuous version (for example, density will use kernel density estimation and x versus y plots will use LOESS regression). For this to work, x needs to be a numeric type. For analysis where smoothing make sense, the smoothing widget allows to pass from thinner to coarser smoothing.Axis type discrete means that the x axis will treat the x values individually. For example, an x versus y plot will plot, for every value of x, the average of the y values for datapoints with the corresponding value of x. Different estimator of y rather than mean (i.e. median) can be chosen below the y axis widget.Axis type binned bins the x data and simply acts like the discrete axis type after the data is binned. Number of bins can be decided using the smoothing slider."
+},
+
+{
+    "location": "getting_started.html#Deciding-how-to-compute-the-variability-1",
+    "page": "Getting Started",
+    "title": "Deciding how to compute the variability",
+    "category": "section",
+    "text": "This section only makes sense if your axis type is not pointbypoint. In this case several ways of estimating variability are proposed:none: do not estimate variability\nacross var where var is one of your colomuns: group by that column, compute the desired function for each group, plot the mean across groups, error is s.e.m. across groups\nacross same as before, except it computes the error across all observation (beware: this does generally not make sense in combination with a continuous axis)\nbootstrap: simulates 1000 fake datasets distributed like yours, computes the desired function on each of them, then plots mean across simulated datasets, error is standard deviation across simulated datasets (see Non-parametric bootstrap)"
+},
+
+{
+    "location": "getting_started.html#Selecting/splitting-data-1",
+    "page": "Getting Started",
+    "title": "Selecting/splitting data",
+    "category": "section",
+    "text": "Splitting data is extremely simple. Variables with less thank nbox = 5 possible values appear as toggle buttons. If toggled, the data will be split on that variable. You can toggle as many of those as you want.To select data, if the variable has few possible values, you'll see all the values listed as a series of checkboxes. Uncheck the values you want to exclude. For continuous data, you are provided two spinboxes that you can use to select the minimum and maximum acceptable values."
+},
+
+{
+    "location": "getting_started.html#Drawing/saving-plots-1",
+    "page": "Getting Started",
+    "title": "Drawing/saving plots",
+    "category": "section",
+    "text": "To draw a plot, simply press the PLOT button. All the valid keywords for Plots.jl can be added in the textbox below the plot, here for example the added keywords are color = [:black :blue], legend = :topleft:(Image: gui)There is an experimental button PLOT! to plot on top of an existing plots, but it's implementation is not very robust and may change.To save the plot, simply press the SAVE button and it will open a saving dialog. The extension you give to the filename will determine its format (i.e. \"myplot.png\" will be saved as png whereas \"myplot.svg\" will be saved in vectorial format)."
+},
+
+{
     "location": "internals.html#",
     "page": "Internals",
     "title": "Internals",
@@ -85,7 +141,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Internals",
     "title": "Internals",
     "category": "section",
-    "text": "Most of the hard work is outsourced to separate packages: mainly QML.jl and StatPlots.jl"
+    "text": "Most of the hard work is outsourced to separate packages: mainly QML.jl, StatPlots.jl and GroupedErrors.jl."
 },
 
 {
@@ -101,7 +157,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Internals",
     "title": "Plotting",
     "category": "section",
-    "text": "Plotting is executed via the StatPlots.jl package. Population plots tap into the groupapply functionality whereas Individual plots have access to most of StatPlots.jl plotting recipes. See the StatPlots README for more information."
+    "text": "Plotting is executed via the StatPlots.jl package. The data manipulation is implemented in GroupedErrors.jl. The GroupedErrors package explains how to obtain the same plots (and some more) with code rather than a GUI."
 },
 
 ]}
