99aa19abad825dd8217e90d65e94af9f7c163148
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2017-10-02 22:12:17 +0100

    restore

--- src/PlugAndPlot.jl
+++ src/PlugAndPlot.jl
@@ -1,7 +1,7 @@
 __precompile__(false)
 module PlugAndPlot
 
-using QML, StatPlots, DataFrames, TextParse#, GroupedErrors, IndexedTables
+using QML, StatPlots, DataFrames, TextParse, GroupedErrors, IndexedTables
 gr()
 
 export choose_data, build_window
@@ -9,5 +9,5 @@ export choose_data, build_window
 include("types.jl")
 include("select_functions.jl")
 include("build_window.jl")
-#include("build_plot.jl")
+include("build_plot.jl")
 end # module
